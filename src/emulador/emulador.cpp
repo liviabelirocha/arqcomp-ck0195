68ed26dc44b209e6e51ecbd5e7ff2dfe912d8342
liviabelirocha <liviabelirocha@outlook.com>
2020-03-14 14:34:57 -0300

    Leitura do microprog.rom

--- src/emulador/emulador.cpp
+++ src/emulador/emulador.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 
@@ -19,7 +21,7 @@ palavra Barramento_B, Barramento_C; //Não há Barramento A pois ele é o própr
 //Próxima Microinstrução
 palavra MPC;
 //Armazenamento de Controle
-microinstrucao ArmazenamentoDeControle[512]; //512 posições de 36 bits
+microinstrucao ArmazenamentoDeControle[512]; //512 posições de 64 bits
 
 //Flip-Flops
 byte N, Z;
@@ -40,15 +42,65 @@ MIR_Barramento_B = 4 bits
 palavra MIR_Prox_MicInstruc, MIR_Barramento_C;
 byte MIR_Salto, MIR_Deslocador, MIR_Operacao, MIR_Memoria, MIR_Barramento_B;
 
-/*Protótipo das funções*/ 
+/*Protótipo das funções*/
+bool carregarArmazenamentoDeControle();
+void atribuicaoMicroinstrucao();
 void atribuicaoB();
-void atribuicaoC();
-void atribuirMicroinstrucao();
 void ALU();
+void atribuicaoC();
 
 /*Main*/
 int main() {
+    if ( carregarArmazenamentoDeControle() ) return 1;
+    int i = 0;
+    while(true) {
+        cout << "Iteração " << i;
+
+        //Microinstrução atual
+        MIR = ArmazenamentoDeControle[MPC];
+
+        //Operações a serem realizadas
+        atribuicaoMicroinstrucao();
+        atribuicaoB();
+        ALU();
+        atribuicaoC();
+
+        //Só vai para próxima instrução quando o "enter" for pressionado
+        i += 1;
+        getchar();
+    }
     return 0;
+}
+
+bool carregarArmazenamentoDeControle() {
+    FILE * armazenamento; //Objeto que contém a informação para controlar uma stream
+    armazenamento = fopen("microprog.rom", "rb"); //rb -> read binary
+    if (armazenamento) { 
+        /*
+        fread ( void * ptr, size_t size, size_t count, FILE * stream );
+        void * ptr -> bloco de memória com tamanho suficiente
+        size_t size -> tamanho em bytes de cada elemento a ser lido, ou seja, cada microinstrução (sizeof(microinstrucao) = 8)
+        size_t count -> número de elementos a ser lido
+        FILE * stream -> objeto especificado anteriormente
+        */
+        fread(ArmazenamentoDeControle, 8, 512, armazenamento);
+        fclose(armazenamento);
+        return false;
+    } else {
+        cout << "(ERRO) Arquivo não existe!" << endl;
+        return true;
+    }
+}
+
+//"Quebra" da microinstrução em várias partes
+void atribuicaoMicroinstrucao() {
+    MIR_Barramento_B = (MIR) & 0b1111;
+    MIR_Memoria = (MIR >> 4) & 0b111;
+    MIR_Barramento_C = (MIR >> 7) & 0b111111111;
+    MIR_Operacao = (MIR >> 16) & 0b111111;
+    MIR_Deslocador = (MIR >> 24) & 0b11;
+    MIR_Salto = (MIR >> 24) & 0b111;
+    MIR_Prox_MicInstruc = (MIR >> 27) & 0b111111111;
 }
 
 //4 bits, variando de 0 a 8
@@ -65,28 +117,6 @@ void atribuicaoB() {
         case 8: Barramento_B  = OPC; break;
         default: Barramento_B = -1;  break;
     }
-}
-
-void atribuicaoC() {
-    if(MIR_Barramento_C & 0b000000001) MAR = Barramento_C;
-	if(MIR_Barramento_C & 0b000000010) MDR = Barramento_C;
-	if(MIR_Barramento_C & 0b000000100) PC  = Barramento_C;
-	if(MIR_Barramento_C & 0b000001000) SP  = Barramento_C;
-	if(MIR_Barramento_C & 0b000010000) LV  = Barramento_C;
-	if(MIR_Barramento_C & 0b000100000) CPP = Barramento_C;
-	if(MIR_Barramento_C & 0b001000000) TOS = Barramento_C;
-	if(MIR_Barramento_C & 0b010000000) OPC = Barramento_C;
-	if(MIR_Barramento_C & 0b100000000) H   = Barramento_C;
-}
-
-void atribuirMicroinstrucao() {
-    MIR_Barramento_B = (MIR) & 0b1111;
-    MIR_Memoria = (MIR >> 4) & 0b111;
-    MIR_Barramento_C = (MIR >> 7) & 0b111111111;
-    MIR_Operacao = (MIR >> 16) & 0b111111;
-    MIR_Deslocador = (MIR >> 24) & 0b11;
-    MIR_Salto = (MIR >> 24) & 0b111;
-    MIR_Prox_MicInstruc = (MIR >> 27) & 0b111111111;
 }
 
 /*
@@ -129,6 +159,18 @@ void ALU() {
         default:                                      break;
     }
 
-    if (Barramento_C) { N = 0; Z = 1; } //Se != 0
-    else { N = 1; Z = 0; };             //Se == 0
+    if (Barramento_C) { N = 1; Z = 0; } //Se != 0
+    else { N = 0; Z = 1; };             //Se == 0
+}
+
+void atribuicaoC() {
+    if(MIR_Barramento_C & 0b000000001) MAR = Barramento_C;
+    if(MIR_Barramento_C & 0b000000010) MDR = Barramento_C;
+    if(MIR_Barramento_C & 0b000000100) PC  = Barramento_C;
+    if(MIR_Barramento_C & 0b000001000) SP  = Barramento_C;
+    if(MIR_Barramento_C & 0b000010000) LV  = Barramento_C;
+    if(MIR_Barramento_C & 0b000100000) CPP = Barramento_C;
+    if(MIR_Barramento_C & 0b001000000) TOS = Barramento_C;
+    if(MIR_Barramento_C & 0b010000000) OPC = Barramento_C;
+    if(MIR_Barramento_C & 0b100000000) H   = Barramento_C;
 }
